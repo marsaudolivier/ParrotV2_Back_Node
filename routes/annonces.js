0112efc7f8525016fb7558bc105ac89c258a919d
Marsaud olivier <cougies79700@gmail.com>
2024-02-01 14:14:26 +0100

    push

--- routes/annonces.js
+++ routes/annonces.js
@@ -24,10 +24,10 @@ router.get('/:id', (req, res) => {
     }
   });
 });
-//recupération des annonces avec inner join voiture Id_voitures + marques + modeles
+//recupération des annonces avec inner join voiture Id_voitures + marques + modeles + consommation ennergie 
 router.get('/voiture/:id', (req, res) => {
   const id = req.params.id;
-  pool.query('SELECT * FROM `Annonces` INNER JOIN Voitures ON Annonces.Id_Voitures = Voitures.Id_Voitures INNER JOIN Marques ON Voitures.Id_Marques = Marques.Id_Marques INNER JOIN Modeles ON Voitures.Id_Modeles = Modeles.Id_Modeles WHERE Annonces.Id_Voitures = ? ', id, (error, results, fields) => {
+  pool.query('SELECT * FROM `Annonces` INNER JOIN comsommer ON Annonces.Id_Voitures = comsommer.Id_Voitures INNER JOIN Energies ON comsommer.Id_Energies = Energies.Id_Energies INNER JOIN Voitures ON Annonces.Id_Voitures = Voitures.Id_Voitures INNER JOIN Marques ON Voitures.Id_Marques = Marques.Id_Marques INNER JOIN Modeles ON Voitures.Id_Modeles = Modeles.Id_Modeles WHERE Annonces.Id_Voitures = ? ', id, (error, results, fields) => {
     if (error) {
       res.json({ message: error.message });
     } else {
