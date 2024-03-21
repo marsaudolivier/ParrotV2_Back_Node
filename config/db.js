2b712b5b69e04ee57d19054f950676bafa2d1194
Marsaud <126662713+marsaudolivier@users.noreply.github.com>
2024-03-21 22:49:50 +0100

    Update db.js
    ad

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host: 'mysql-marsaudolivier.alwaysdata.net',
-  user: '315610_test',
-  password: 'Mapadis1701',
-  database: 'marsaudolivier_garageparrot2',
+  host: '****',
+  user: '****',
+  password: '*****',
+  database: '******',
   connectionLimit: 10,
 });
 
